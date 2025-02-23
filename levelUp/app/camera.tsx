3b6db047c41c317b1727eef2c0c748f8dd037674
Vaishnavi Sen <vaishusen2002@gmail.com>
2025-02-23 10:04:16 -0800

    hard code removed

--- levelUp/app/camera.tsx
+++ levelUp/app/camera.tsx
@@ -66,7 +66,7 @@ export default function App() {
       if (response.ok) {
         navigation.navigate("quiz", { data: result.success });
       } else {
-        navigation.navigate("quiz", { data: true });
+        navigation.navigate("quiz", { data: result.success });
       }
     } catch (error) {
       console.error("Error uploading image:", error);
